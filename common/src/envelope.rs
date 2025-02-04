a710992b4a9c78ec61d1fd5fb5378ef2931c877a
Alexander Steffensen <74182126+alexsteffensen@users.noreply.github.com>
2025-02-04 15:18:17 +0100

    Added protobuf definitions and tests (#4)
    * Added protobuf definitions and tests
    
    * Cargo fmt and cargo clippy
    
    * Reformatted protobuf

---
+++ common/src/envelope.rs
@@ -0,0 +1,42 @@
+#[cfg(test)]
+mod envelope_test {
+    use crate::sam_message::MessageType;
+    use crate::sam_message::{ClientEnvelope, ServerEnvelope};
+    use std::collections::HashMap;
+
+    #[test]
+    fn client_envelope_test() {
+        let envelope: ClientEnvelope = ClientEnvelope {
+            r#type: MessageType::SignalMessage.into(),
+            destination: "Magnus".to_string(),
+            source: "Alex".to_string(),
+            content: HashMap::from([
+                (1, vec![10, 20, 30]),
+                (2, vec![40, 50, 60]),
+                (3, vec![70, 80, 90]),
+            ]),
+        };
+
+        assert_eq!(envelope.r#type, MessageType::SignalMessage.into());
+        assert_eq!(envelope.destination, "Magnus".to_string());
+        assert_eq!(envelope.source, "Alex".to_string());
+        assert_eq!(envelope.content.get(&1), Some(&vec![10, 20, 30]));
+        assert_eq!(envelope.content.get(&2), Some(&vec![40, 50, 60]));
+        assert_eq!(envelope.content.get(&3), Some(&vec![70, 80, 90]));
+    }
+
+    #[test]
+    fn server_envelope_test() {
+        let envelope: ServerEnvelope = ServerEnvelope {
+            r#type: MessageType::SignalMessage.into(),
+            destination: "Magnus".to_string(),
+            source: "Alex".to_string(),
+            content: vec![10, 20, 30],
+        };
+
+        assert_eq!(envelope.r#type, MessageType::SignalMessage.into());
+        assert_eq!(envelope.destination, "Magnus".to_string());
+        assert_eq!(envelope.source, "Alex".to_string());
+        assert_eq!(envelope.content, vec![10, 20, 30]);
+    }
+}
