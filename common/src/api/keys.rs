5d267d9b72df0609b6d9b8b0cb63d8a0a137020e
Simon Deleuran Laursen <69715502+Laursen79@users.noreply.github.com>
2025-02-12 11:32:32 +0100

    added base64 to IdentityKey, SignedPreKeyRecord can no longer .into() PostQuantumPreKey, Key is no longer Sized

--- common/src/api/keys.rs
+++ common/src/api/keys.rs
@@ -15,15 +15,6 @@ macro_rules! define_key {
             pub public_key: Box<[u8]>,
         }
 
-        impl From<PreKeyRecord> for $name {
-            fn from(value: PreKeyRecord) -> Self {
-                $name {
-                    key_id: value.id().expect("Can get ID").into(),
-                    public_key: value.public_key().expect("Can get public_key").serialize(),
-                }
-            }
-        }
-
         impl Key for $name {
             fn id(&self) -> u32 {
                 return self.key_id;
@@ -43,29 +34,9 @@ macro_rules! define_signed_key {
         pub struct $name {
             pub key_id: u32,
             #[serde_as(as = "Base64")]
-            pub public_key: Box<[u8]>, // TODO: Make this a PublicKey and implement Serialize
+            pub public_key: Box<[u8]>,
             #[serde_as(as = "Base64")]
-            pub signature: Box<[u8]>, // TODO: Make this a PublicKey and implement Serialize
-        }
-
-        impl From<SignedPreKeyRecord> for $name {
-            fn from(value: SignedPreKeyRecord) -> Self {
-                $name {
-                    key_id: value.id().expect("Can get ID").into(),
-                    public_key: value.public_key().expect("Can get public_key").serialize(),
-                    signature: value.signature().expect("Can get signature").into(),
-                }
-            }
-        }
-
-        impl From<KyberPreKeyRecord> for $name {
-            fn from(value: KyberPreKeyRecord) -> Self {
-                $name {
-                    key_id: value.id().expect("Can get ID").into(),
-                    public_key: value.public_key().expect("Can get public_key").serialize(),
-                    signature: value.signature().expect("Can get signature").into(),
-                }
-            }
+            pub signature: Box<[u8]>,
         }
 
         impl Key for $name {
@@ -85,7 +56,7 @@ macro_rules! define_signed_key {
     };
 }
 
-pub trait Key: Sized {
+pub trait Key {
     fn id(&self) -> u32;
     fn public_key(&self) -> &[u8];
 }
@@ -94,13 +65,44 @@ pub trait SignedKey: Key {
     fn signature(&self) -> &[u8];
 }
 
-define_key!(PreKey);
+define_key!(EllipticCurvePreKey);
+
+impl From<PreKeyRecord> for EllipticCurvePreKey {
+    fn from(value: PreKeyRecord) -> Self {
+        Self {
+            key_id: value.id().expect("Can get ID").into(),
+            public_key: value.public_key().expect("Can get public_key").serialize(),
+        }
+    }
+}
+
 define_signed_key!(SignedPreKey);
+
+impl From<SignedPreKeyRecord> for SignedPreKey {
+    fn from(value: SignedPreKeyRecord) -> Self {
+        Self {
+            key_id: value.id().expect("Can get ID").into(),
+            public_key: value.public_key().expect("Can get public_key").serialize(),
+            signature: value.signature().expect("Can get signature").into(),
+        }
+    }
+}
+
 define_signed_key!(PostQuantumPreKey);
+
+impl From<KyberPreKeyRecord> for PostQuantumPreKey {
+    fn from(value: KyberPreKeyRecord) -> Self {
+        Self {
+            key_id: value.id().expect("Can get ID").into(),
+            public_key: value.public_key().expect("Can get public_key").serialize(),
+            signature: value.signature().expect("Can get signature").into(),
+        }
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct BundleResponse {
+pub struct KeyBundleResponse {
     #[serde(with = "id_key")]
     pub identity_key: IdentityKey,
     pub bundles: Vec<KeyBundle>,
@@ -111,7 +113,7 @@ pub struct BundleResponse {
 pub struct KeyBundle {
     pub device_id: u32,
     pub registration_id: u32,
-    pub pre_key: Option<PreKey>,
+    pub pre_key: Option<EllipticCurvePreKey>,
     pub pq_pre_key: PostQuantumPreKey,
     pub signed_pre_key: SignedPreKey,
 }
@@ -119,7 +121,7 @@ pub struct KeyBundle {
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct PublishKeyBundleRequest {
-    pub pre_keys: Option<Vec<PreKey>>,
+    pub pre_keys: Option<Vec<EllipticCurvePreKey>>,
     pub signed_pre_key: Option<SignedPreKey>,
     pub pq_pre_keys: Option<Vec<PostQuantumPreKey>>,
     pub pq_last_resort_pre_key: Option<PostQuantumPreKey>,
@@ -128,6 +130,7 @@ pub struct PublishKeyBundleRequest {
 pub type PublishKeyBundle = PublishKeyBundleRequest;
 
 pub mod id_key {
+    use base64::{prelude::BASE64_STANDARD, Engine as _};
     use libsignal_protocol::IdentityKey;
     use serde::{self, Deserialize, Deserializer, Serializer};
 
@@ -136,7 +139,8 @@ pub mod id_key {
         S: Serializer,
     {
         // Convert IdentityKey to bytes and serialize them
-        serializer.serialize_bytes(&key.serialize())
+
+        serializer.serialize_str(&BASE64_STANDARD.encode(key.serialize()))
     }
 
     pub fn deserialize<'de, D>(deserializer: D) -> Result<IdentityKey, D::Error>
@@ -144,7 +148,9 @@ pub mod id_key {
         D: Deserializer<'de>,
     {
         use serde::de::Error;
-        let bytes = Vec::<u8>::deserialize(deserializer)?;
+        let bytes = BASE64_STANDARD
+            .decode(String::deserialize(deserializer)?)
+            .map_err(|err| Error::custom(format!("{err}")))?;
 
         IdentityKey::decode(&bytes)
             .map_err(|e| Error::custom(format!("Failed to decode IdentityKey: {}", e)))
--- common/src/api/mod.rs
+++ common/src/api/mod.rs
@@ -1,3 +1,12 @@
 pub mod account;
 pub mod device;
 pub mod keys;
+
+pub use account::{RegistrationRequest, RegistrationResponse};
+
+pub use device::{LinkDeviceRequest, LinkDeviceResponse, LinkDeviceToken};
+
+pub use keys::{
+    EllipticCurvePreKey, Key, KeyBundleResponse, PostQuantumPreKey, PublishKeyBundleRequest,
+    SignedKey, SignedPreKey,
+};
