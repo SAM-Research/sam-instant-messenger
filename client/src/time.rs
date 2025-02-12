809c8f1ca51f193e4de97b5c89693fbd19d08308
Magnus J. Harder <26723180+MJHC@users.noreply.github.com>
2025-02-12 12:41:58 +0100

    Common api types (#17)
    * api-types
    
    * moved time_now to client, renamed time_now_128
    
    ---------
    
    Co-authored-by: Simon Deleuran Laursen <69715502+Laursen79@users.noreply.github.com>

---
+++ client/src/time.rs
@@ -0,0 +1,9 @@
+use libsignal_protocol::Timestamp;
+use sam_common::time_now_millis;
+pub fn signal_time_now() -> Timestamp {
+    Timestamp::from_epoch_millis(
+        time_now_millis()
+            .try_into()
+            .expect("Living in the future is not allowed"),
+    )
+}
