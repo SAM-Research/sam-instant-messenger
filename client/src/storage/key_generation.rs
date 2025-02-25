fa62c4cd6c6537820ff941d76851de9f85240035
Alexander Steffensen <74182126+alexsteffensen@users.noreply.github.com>
2025-02-25 12:59:09 +0100

    Implemented new version of key generation (#34)
    * implemented new version of key generation
    
    * fixed warnings
    
    * changed tests
    
    * Changed generation to generator
    
    * fixed comments
    
    * fixed cspring
    
    * fixed comments
    
    * fixed import

---
+++ client/src/storage/key_generation.rs
@@ -0,0 +1,155 @@
+use crate::storage::ProvidesKeyId;
+use crate::{signal_time_now, ClientError};
+use async_trait::async_trait;
+use libsignal_core::curve::{KeyPair, PrivateKey};
+use libsignal_protocol::kem::KeyType;
+use libsignal_protocol::{
+    GenericSignedPreKey, KyberPreKeyId, KyberPreKeyRecord, KyberPreKeyStore, PreKeyId,
+    PreKeyRecord, PreKeyStore, SignedPreKeyId, SignedPreKeyRecord, SignedPreKeyStore,
+};
+use rand::{CryptoRng, Rng};
+
+#[async_trait(?Send)]
+pub trait PreKeyGenerator {
+    async fn generate_key<R: Rng + CryptoRng>(
+        &mut self,
+        csprng: &mut R,
+    ) -> Result<PreKeyRecord, ClientError>;
+}
+
+#[async_trait(?Send)]
+impl<T: PreKeyStore + ProvidesKeyId<PreKeyId>> PreKeyGenerator for T {
+    async fn generate_key<R>(&mut self, csprng: &mut R) -> Result<PreKeyRecord, ClientError>
+    where
+        R: Rng + CryptoRng,
+    {
+        let id = self.next_key_id().await?;
+
+        let key_pair = KeyPair::generate(csprng);
+        let record = PreKeyRecord::new(id, &key_pair);
+        self.save_pre_key(id, &record).await?;
+        Ok(record)
+    }
+}
+
+#[async_trait(?Send)]
+pub trait SignedPreKeyGenerator {
+    async fn generate_key<R: Rng + CryptoRng>(
+        &mut self,
+        csprng: &mut R,
+        private_key: &PrivateKey,
+    ) -> Result<SignedPreKeyRecord, ClientError>;
+}
+
+#[async_trait(?Send)]
+impl<T: SignedPreKeyStore + ProvidesKeyId<SignedPreKeyId>> SignedPreKeyGenerator for T {
+    async fn generate_key<R>(
+        &mut self,
+        csprng: &mut R,
+        private_key: &PrivateKey,
+    ) -> Result<SignedPreKeyRecord, ClientError>
+    where
+        R: Rng + CryptoRng,
+    {
+        let id = self.next_key_id().await?;
+        let signed_pre_key_pair = KeyPair::generate(csprng);
+        let signature =
+            private_key.calculate_signature(&signed_pre_key_pair.public_key.serialize(), csprng)?;
+
+        let record =
+            SignedPreKeyRecord::new(id, signal_time_now(), &signed_pre_key_pair, &signature);
+
+        self.save_signed_pre_key(id, &record).await?;
+
+        Ok(record)
+    }
+}
+
+#[async_trait(?Send)]
+pub trait KyberKeyGenerator {
+    async fn generate_key(
+        &mut self,
+        private_key: &PrivateKey,
+    ) -> Result<KyberPreKeyRecord, ClientError>;
+}
+
+#[async_trait(?Send)]
+impl<T: KyberPreKeyStore + ProvidesKeyId<KyberPreKeyId>> KyberKeyGenerator for T {
+    async fn generate_key(
+        &mut self,
+        private_key: &PrivateKey,
+    ) -> Result<KyberPreKeyRecord, ClientError> {
+        let id = self.next_key_id().await?;
+        let record = KyberPreKeyRecord::generate(KeyType::Kyber1024, id, private_key)?;
+
+        self.save_kyber_pre_key(id, &record).await?;
+        Ok(record)
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+    use libsignal_protocol::{
+        IdentityKeyPair, InMemKyberPreKeyStore, InMemPreKeyStore, InMemSignedPreKeyStore,
+    };
+    use rand::rngs::OsRng;
+
+    #[tokio::test]
+    async fn inmem_pre_key_generation() {
+        let mut inmem_keystore = InMemPreKeyStore::new();
+        let mut rng = OsRng;
+        let returned_key = inmem_keystore.generate_key(&mut rng).await.unwrap();
+        let stored_key = inmem_keystore
+            .get_pre_key(returned_key.id().unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(
+            returned_key.public_key().unwrap(),
+            stored_key.public_key().unwrap()
+        );
+    }
+
+    #[tokio::test]
+    async fn inmem_signed_pre_key_generation() {
+        let mut inmem_keystore = InMemSignedPreKeyStore::new();
+        let mut rng = OsRng;
+        let identity_key_pair = IdentityKeyPair::generate(&mut rng);
+        let returned_key = inmem_keystore
+            .generate_key(&mut rng, identity_key_pair.private_key())
+            .await
+            .unwrap();
+        let stored_key = inmem_keystore
+            .get_signed_pre_key(returned_key.id().unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(
+            returned_key.public_key().unwrap(),
+            stored_key.public_key().unwrap()
+        );
+        assert_eq!(
+            returned_key.signature().unwrap(),
+            stored_key.signature().unwrap()
+        );
+    }
+
+    #[tokio::test]
+    async fn inmem_kyber_pre_key_generation() {
+        let mut inmem_keystore = InMemKyberPreKeyStore::new();
+        let identity_key_pair = IdentityKeyPair::generate(&mut OsRng);
+        let returned_key = inmem_keystore
+            .generate_key(identity_key_pair.private_key())
+            .await
+            .unwrap();
+        let stored_key = inmem_keystore
+            .get_kyber_pre_key(returned_key.id().unwrap())
+            .await
+            .unwrap()
+            .signature()
+            .unwrap();
+
+        assert_eq!(returned_key.signature().unwrap(), stored_key);
+    }
+}
