dc416456ce44e48f31018fe4644b547bc89f24ab
Magnus Harder <26723180+MJHC@users.noreply.github.com>
2025-02-28 14:15:28 +0100

    added more tests

--- client/src/protocol/client.rs
+++ client/src/protocol/client.rs
@@ -183,14 +183,18 @@ mod test {
 
     use futures_util::{SinkExt, StreamExt};
     use prost::{bytes::Bytes, Message as PMessage};
+    use rstest::rstest;
     use sam_common::{
         address::{AccountId, MessageId},
         sam_message::{
             ClientEnvelope, ClientMessage, EnvelopeType, MessageType, ServerEnvelope, ServerMessage,
         },
     };
-    use tokio::net::TcpListener;
-    use tokio_tungstenite::{accept_async, tungstenite::Message};
+    use tokio::{
+        net::{TcpListener, TcpStream},
+        sync::oneshot::{self, Receiver, Sender},
+    };
+    use tokio_tungstenite::{accept_async, tungstenite::Message, WebSocketStream};
 
     use crate::protocol::{
         client::ProtocolClient,
@@ -198,58 +202,197 @@ mod test {
         websocket::{WebSocketClient, WebSocketClientConfig},
     };
 
-    async fn test_server(addr: String) {
+    fn server_env(id: MessageId) -> ServerMessage {
+        ServerMessage::builder()
+            .id(id.clone().into())
+            .r#type(MessageType::Message as i32)
+            .message(
+                ServerEnvelope::builder()
+                    .id(id.into())
+                    .r#type(EnvelopeType::PlaintextContent as i32)
+                    .content(vec![1, 2, 3])
+                    .destination_account_id(AccountId::generate().into())
+                    .destination_device_id(1u32)
+                    .source_account_id(AccountId::generate().into())
+                    .source_device_id(1u32)
+                    .build(),
+            )
+            .build()
+    }
+
+    fn server_ack(id: Vec<u8>) -> ServerMessage {
+        ServerMessage::builder()
+            .id(id)
+            .r#type(MessageType::Ack as i32)
+            .build()
+    }
+
+    async fn send(stream: &mut WebSocketStream<TcpStream>, msg: ServerMessage) -> Result<(), ()> {
+        stream
+            .send(Message::Binary(msg.encode_to_vec().into()))
+            .await
+            .map_err(|_| ())
+    }
+
+    fn decode_client_msg(bytes: Bytes) -> Result<ClientMessage, ()> {
+        ClientMessage::decode(Bytes::from(bytes)).map_err(|_| ())
+    }
+
+    fn client_env() -> ClientEnvelope {
+        ClientEnvelope::builder()
+            .r#type(MessageType::Message as i32)
+            .content(HashMap::new())
+            .destination_account_id(AccountId::generate().into())
+            .source_device_id(1u32)
+            .source_account_id(AccountId::generate().into())
+            .build()
+    }
+
+    fn oneshot(tx: &mut Option<Sender<Result<(), String>>>, msg: Result<(), String>) {
+        if let Some(tx) = tx.take() {
+            let _ = tx.send(msg);
+        }
+    }
+
+    #[derive(Clone)]
+    enum ServerAction {
+        Receive,
+        Send,
+    }
+
+    async fn test_server(addr: String, msg_seq: Vec<ServerAction>) -> Receiver<Result<(), String>> {
         let listener = TcpListener::bind(addr).await.unwrap();
 
+        let (tx, rx) = oneshot::channel();
         tokio::spawn(async move {
             let (stream, _) = listener.accept().await.unwrap();
             let mut ws_stream = accept_async(stream).await.unwrap();
+            let mut tx = Some(tx);
+            for (i, action) in msg_seq.iter().enumerate() {
+                match action {
+                    ServerAction::Send => {
+                        let id = MessageId::generate();
+                        let timeout = tokio::time::timeout(
+                            Duration::from_millis(300),
+                            send(&mut ws_stream, server_env(id.clone())),
+                        )
+                        .await;
+                        let res = match timeout {
+                            Ok(res) => res,
+                            Err(_) => {
+                                oneshot(
+                                    &mut tx,
+                                    Err("Client failed to send in time interval".to_string()),
+                                );
+                                break;
+                            }
+                        };
+                        match res {
+                            Ok(_) => {}
+                            Err(_) => {
+                                oneshot(&mut tx, Err("Failed to send".to_string()));
+                                break;
+                            }
+                        }
+                        if let Some(Ok(msg)) = ws_stream.next().await {
+                            let msg_res = match msg {
+                                Message::Binary(b) => decode_client_msg(b),
+                                _ => return,
+                            };
 
-            if let Some(Ok(msg)) = ws_stream.next().await {
-                let id = match msg {
-                    Message::Binary(b) => {
-                        ClientMessage::decode(Bytes::from(b))
-                            .expect("Can decode client message")
-                            .id
+                            let msg = match msg_res {
+                                Ok(msg) => msg,
+                                Err(_) => {
+                                    oneshot(
+                                        &mut tx,
+                                        Err("Failed to decode client message when receiving"
+                                            .to_string()),
+                                    );
+                                    break;
+                                }
+                            };
+
+                            if msg.r#type() != MessageType::Ack {
+                                oneshot(
+                                    &mut tx,
+                                    Err("Expected Ack message got something else".to_string()),
+                                );
+                                break;
+                            }
+
+                            if id.into_bytes() != *msg.id {
+                                oneshot(
+                                    &mut tx,
+                                    Err("Ack Id is not the same as sent Id".to_string()),
+                                );
+                                break;
+                            }
+                        }
                     }
-                    _ => return,
-                };
-                let server_msg = ServerMessage::builder()
-                    .id(id)
-                    .r#type(MessageType::Ack as i32)
-                    .build();
-                let env_id = MessageId::generate();
-                let env_msg = ServerMessage::builder()
-                    .id(env_id.into())
-                    .r#type(MessageType::Message as i32)
-                    .message(
-                        ServerEnvelope::builder()
-                            .id(env_id.into())
-                            .r#type(EnvelopeType::PlaintextContent as i32)
-                            .content(vec![1, 2, 3])
-                            .destination_account_id(AccountId::generate().into())
-                            .destination_device_id(1u32)
-                            .source_account_id(AccountId::generate().into())
-                            .source_device_id(1u32)
-                            .build(),
-                    )
-                    .build();
-                ws_stream
-                    .send(Message::Binary(server_msg.encode_to_vec().into()))
-                    .await
-                    .expect("Can send message");
-                ws_stream
-                    .send(Message::Binary(env_msg.encode_to_vec().into()))
-                    .await
-                    .expect("Can send message");
+                    ServerAction::Receive => {
+                        if let Some(Ok(msg)) = ws_stream.next().await {
+                            let msg_res = match msg {
+                                Message::Binary(b) => decode_client_msg(b),
+                                _ => return,
+                            };
+
+                            let id = match msg_res {
+                                Ok(msg) => msg.id,
+                                Err(_) => {
+                                    oneshot(
+                                        &mut tx,
+                                        Err("Failed to decode client message when receiving"
+                                            .to_string()),
+                                    );
+                                    break;
+                                }
+                            };
+
+                            let timeout = tokio::time::timeout(
+                                Duration::from_millis(300),
+                                send(&mut ws_stream, server_ack(id)),
+                            )
+                            .await;
+                            let res = match timeout {
+                                Ok(res) => res,
+                                Err(_) => {
+                                    oneshot(
+                                        &mut tx,
+                                        Err("Client failed to send in time interval".to_string()),
+                                    );
+                                    break;
+                                }
+                            };
+                            match res {
+                                Ok(_) => {}
+                                Err(_) => {
+                                    oneshot(&mut tx, Err("Failed to send".to_string()));
+                                    break;
+                                }
+                            };
+                        }
+                    }
+                }
+                if i == msg_seq.len() - 1 {
+                    oneshot(&mut tx, Ok(()));
+                }
             }
         });
+        rx
     }
 
+    #[rstest]
+    #[case(vec![ServerAction::Send, ServerAction::Send], "9081")]
+    #[case(vec![ServerAction::Receive, ServerAction::Receive],"9082")]
+    #[case(vec![ServerAction::Receive, ServerAction::Send], "9083")]
+    #[case(vec![ServerAction::Send, ServerAction::Receive], "9084")]
     #[tokio::test]
-    async fn test_send_and_ack_and_envelope() {
-        let addr = "127.0.0.1:9081".to_string();
-        test_server(addr.clone()).await;
+    async fn test_send_and_ack_and_envelope(
+        #[case] actions: Vec<ServerAction>,
+        #[case] port: String,
+    ) {
+        let addr = format!("127.0.0.1:{}", port);
+        let shutdown = test_server(addr.clone(), actions.clone()).await;
         let client: WebSocketClient = WebSocketClientConfig::builder()
             .url(format!("ws://{}", addr))
             .build()
@@ -258,20 +401,26 @@ mod test {
         let mut client = ProtocolClient::new(client);
         let mut receiver = client.connect().await.expect("Can connect");
 
-        let msg = ClientEnvelope::builder()
-            .r#type(MessageType::Message as i32)
-            .content(HashMap::new())
-            .destination_account_id(AccountId::generate().into())
-            .source_device_id(1u32)
-            .source_account_id(AccountId::generate().into())
-            .build();
-        client
-            .send_message(msg)
-            .await
-            .expect("Can send and receive Message");
-        let envelope = tokio::time::timeout(Duration::from_millis(300), receiver.recv())
-            .await
-            .expect("Server responds");
-        assert!(envelope.is_some());
+        let mut results = vec![];
+        for action in actions {
+            let ok = match action {
+                ServerAction::Send => {
+                    tokio::time::timeout(Duration::from_millis(300), receiver.recv())
+                        .await
+                        .is_ok()
+                }
+                ServerAction::Receive => client.send_message(client_env()).await.is_ok(),
+            };
+            results.push(ok);
+            if !ok {
+                break;
+            }
+        }
+
+        assert!(results.iter().all(|x| *x));
+        let timeout = tokio::time::timeout(Duration::from_millis(300), shutdown).await;
+        let recv_res = timeout.expect("Server shutsdown");
+        let res = recv_res.expect("Oneshot works");
+        res.expect("Client server comms works");
     }
 }
