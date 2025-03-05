f901fddb52cf83150dd7ab11ba5aa7906bc70623
Simon Deleuran Laursen <69715502+Laursen79@users.noreply.github.com>
2025-03-05 13:45:17 +0100

    made fns pub on client

--- client/src/client.rs
+++ client/src/client.rs
@@ -9,12 +9,12 @@ use crate::{
         protocol::traits::{ProtocolConfig, SamProtocolClient},
         ApiClient,
     },
-    storage::{Store, StoreConfig, StoreType},
+    storage::{AccountStore, Store, StoreConfig, StoreType},
     ClientError,
 };
 
 pub struct Client<T: StoreType, U: ApiClient, V: SamProtocolClient> {
-    _store: Store<T>,
+    store: Store<T>,
     _api_client: U,
     _protocol_client: V,
 }
@@ -23,7 +23,7 @@ pub struct Client<T: StoreType, U: ApiClient, V: SamProtocolClient> {
 impl<T: StoreType, U: ApiClient, V: SamProtocolClient> Client<T, U, V> {
     /// Creates a new client for the account described in the token
     #[builder]
-    async fn from_provisioning(
+    pub async fn from_provisioning(
         _store_config: impl StoreConfig<StoreType = T>,
         _protocol_config: impl ProtocolConfig,
         _api_client_config: impl ApiClientConfig,
@@ -36,7 +36,7 @@ impl<T: StoreType, U: ApiClient, V: SamProtocolClient> Client<T, U, V> {
 
     /// Register a new account from a clean store
     #[builder]
-    async fn from_registration(
+    pub async fn from_registration(
         _store_config: impl StoreConfig<StoreType = T>,
         _protocol_config: impl ProtocolConfig,
         _api_client_config: impl ApiClientConfig,
@@ -49,36 +49,40 @@ impl<T: StoreType, U: ApiClient, V: SamProtocolClient> Client<T, U, V> {
 
     /// Instantiate a client from a valid store
     #[builder]
-    async fn from_store(
+    pub async fn from_store(
         store: Store<T>,
         protocol_config: impl ProtocolConfig<ProtocolClient = V>,
         api_client_config: impl ApiClientConfig<ApiClient = U>,
     ) -> Result<Self, ClientError> {
         Ok(Self {
-            _store: store,
+            store,
             _protocol_client: protocol_config.create().await?,
             _api_client: api_client_config.create().await?,
         })
     }
 
+    pub async fn account_id(&self) -> Result<AccountId, ClientError> {
+        self.store.account_store.get_account_id().await
+    }
+
     /// Delete Account and consume client
-    async fn delete_account(self) {
+    pub async fn delete_account(self) {
         todo!()
     }
 
     /// Delete device and consume client
-    async fn delete_device(self) {
+    pub async fn delete_device(self) {
         todo!()
     }
 
     /// Get users account id from username
-    async fn get_user_account_id(&self, _username: &str) -> Result<AccountId, ClientError> {
+    pub async fn get_user_account_id(&self, _username: &str) -> Result<AccountId, ClientError> {
         todo!()
     }
 
     /// Send any message to receipient
     /// Should also send to users other devices
-    async fn send_message(
+    pub async fn send_message(
         &mut self,
         _receipient: AccountId,
         _msg: impl Into<Vec<u8>>,
@@ -87,13 +91,13 @@ impl<T: StoreType, U: ApiClient, V: SamProtocolClient> Client<T, U, V> {
     }
 
     /// Returns a broadcast receiver for incoming messages that have been decrypted
-    async fn subscribe(&mut self) -> Result<Receiver<DecryptedEnvelope>, ClientError> {
+    pub async fn subscribe(&mut self) -> Result<Receiver<DecryptedEnvelope>, ClientError> {
         todo!()
     }
 
     /// publish ec, pq, last resort or last resort of amount
     #[builder]
-    async fn publish_prekeys(
+    pub async fn publish_prekeys(
         &mut self,
         #[builder(default)] _onetime_ec_keys: u32,
         #[builder(default)] _onetime_pq_prekeys: u32,
@@ -104,7 +108,7 @@ impl<T: StoreType, U: ApiClient, V: SamProtocolClient> Client<T, U, V> {
     }
 
     /// Fetch key bundles for account_id
-    async fn fetch_prekeys(
+    pub async fn fetch_prekeys(
         &mut self,
         _account_id: AccountId,
         _devices: Vec<DeviceId>,
@@ -113,7 +117,7 @@ impl<T: StoreType, U: ApiClient, V: SamProtocolClient> Client<T, U, V> {
     }
 
     /// Create a provision token to be used on another client to activate
-    async fn create_provision(&mut self) -> Result<LinkDeviceToken, ClientError> {
+    pub async fn create_provision(&mut self) -> Result<LinkDeviceToken, ClientError> {
         todo!()
     }
 }
