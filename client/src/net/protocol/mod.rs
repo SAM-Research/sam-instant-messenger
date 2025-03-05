1973e01c9bee34da156acdf022ca669508f05e39
Magnus J. Harder <26723180+MJHC@users.noreply.github.com>
2025-03-05 13:53:48 +0100

    default config (#53)

--- client/src/net/protocol/mod.rs
+++ client/src/net/protocol/mod.rs
@@ -1,4 +1,25 @@
+use client::ProtocolClient;
+use traits::ProtocolConfig;
+use websocket::WebSocketClientConfig;
+
 pub mod client;
 pub mod error;
 pub mod traits;
 mod websocket;
+
+struct WebSocketProtocolClientConfig {
+    url: String,
+}
+
+#[async_trait::async_trait(?Send)]
+impl ProtocolConfig for WebSocketProtocolClientConfig {
+    type ProtocolClient = ProtocolClient;
+
+    async fn create(self) -> Result<Self::ProtocolClient, error::ProtocolError> {
+        let ws_client = WebSocketClientConfig::builder()
+            .url(format!("ws://{}", self.url))
+            .build()
+            .into();
+        Ok(ProtocolClient::new(ws_client))
+    }
+}
