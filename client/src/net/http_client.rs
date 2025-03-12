2cc4037b3c14ecfcabfca9e4806e192044024e57
Laursen79 <69715502+Laursen79@users.noreply.github.com>
2025-03-12 09:21:28 +0100

    Devices can be linked and unlinked, accounts can be deleted (#61)

--- client/src/net/http_client.rs
+++ client/src/net/http_client.rs
@@ -206,8 +206,7 @@ impl ApiClient for HttpClient {
 
     async fn link_device(
         &self,
-        account_id: AccountId,
-        device_id: DeviceId,
+        username: &str,
         password: &str,
         request: LinkDeviceRequest,
     ) -> Result<LinkDeviceResponse, ApiClientError> {
@@ -218,7 +217,7 @@ impl ApiClient for HttpClient {
             .http_client
             .request(Method::POST, url)
             .json(&request)
-            .basic_auth(format!("{}.{}", account_id, device_id), Some(password))
+            .basic_auth(username.to_owned(), Some(password))
             .build()
             .map_err(|_| ApiClientError::CouldNotBuildRequest)?;
 
