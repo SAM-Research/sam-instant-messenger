affddbec27ad61f0746d4e5ffb897e76fbe50be5
alexsteffensen <asteff20@student.aau.dk>
2025-04-08 12:29:18 +0200

    refactored send message

--- client/src/logic/message.rs
+++ client/src/logic/message.rs
@@ -1,6 +1,9 @@
 use log::error;
 use rand::{CryptoRng, Rng};
-use sam_common::{sam_message::ServerEnvelope, AccountId};
+use sam_common::{
+    sam_message::{ClientEnvelope, ServerEnvelope},
+    AccountId,
+};
 use tokio::sync::mpsc::Receiver;
 
 use crate::{
@@ -50,14 +53,13 @@ pub async fn process_messages<T: StoreType>(
     Ok(())
 }
 
-pub async fn send_message<T: StoreType, R: Rng + CryptoRng>(
+pub async fn prepare_message<T: StoreType, R: Rng + CryptoRng>(
     store: &mut Store<T>,
     api_client: &impl ApiClient,
-    ws_client: &mut impl SamProtocolClient,
     recipient: AccountId,
     msg: impl Into<Vec<u8>>,
     mut rng: &mut R,
-) -> Result<(), ClientError> {
+) -> Result<ClientEnvelope, ClientError> {
     if !store.contact_store.contains_contact(recipient).await? {
         fetch_prekeys(store, api_client, recipient, None, &mut rng).await?;
     }
@@ -67,7 +69,15 @@ pub async fn send_message<T: StoreType, R: Rng + CryptoRng>(
         fetch_prekeys(store, api_client, my_id, None, &mut rng).await?;
     }
     let envelope = encrypt(msg, vec![recipient, my_id], store).await?;
-    let status = ws_client.send_message(envelope).await?;
+    Ok(envelope)
+}
+
+pub async fn handle_message_response<T: StoreType, R: Rng + CryptoRng>(
+    store: &mut Store<T>,
+    api_client: &impl ApiClient,
+    mut rng: &mut R,
+    status: MessageStatus,
+) -> Result<(), ClientError> {
     match status {
         MessageStatus::ExtraDevices(device_lists) => {
             for list in device_lists {
